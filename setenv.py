4d672cbcde8cecde2cfcfac6e2ccd907feb26ed1
canoziia <canoziia@qq.com>
2022-11-24 14:55:18 +0800

    fix bug

--- setenv.py
+++ setenv.py
@@ -3,19 +3,19 @@ import yaml
 
 ConfigPath = os.getenv("CONFIG_PATH")
 AllowFields = {
-    "IPV6": "ipv6",
-    "SOCKS_PORT": "socks-port",
-    "HTTP_PORT": "port",
-    "MIXED_PORT": "mixed-port",
-    "EXTERNAL_CONTROLLER": "external-controller",
-    "ALLOW_LAN": "allow-lan"
+    "IPV6": {"name": "ipv6", "type": bool},
+    "SOCKS_PORT": {"name": "socks-port", "type": int},
+    "HTTP_PORT": {"name": "port", "type": int},
+    "MIXED_PORT": {"name": "mixed-port", "type": int},
+    "EXTERNAL_CONTROLLER": {"name": "external-controller", "type": str},
+    "ALLOW_LAN": {"name": "allow-lan", "type": bool}
 }
 
 with open(ConfigPath, "r") as f:
     config = yaml.safe_load(f)
     for key, value in AllowFields.items():
         if os.getenv(key):
-            config[value] = os.getenv(key)
+            config[value["name"]] = value["type"](os.getenv(key))
 
 with open(ConfigPath, "w") as f:
     yaml.safe_dump(config, f, allow_unicode=True, default_flow_style=False)
